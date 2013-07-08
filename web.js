06bc71f182221d9c1be89c1fe5b710d576f8b1b8
Varren <varren@gmail.com>
2013-07-09 02:33:00 +0400

    Try2

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var fs - require('fs');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var indexData = fs.readFileSync('index.html');
+var indexData = fs.readFileSync('index.html', 'utf8');
 
 
 app.get('/', function(request, response) {
-  response.send(indexData.toString('utf8'));
+  response.send(indexData);
 });
 
 var port = process.env.PORT || 5000;
