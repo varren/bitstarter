e2f1fdbbcfcbc1dcb6d8b5801f2a87e1c1d34010
Varren <varren@gmail.com>
2013-07-09 02:23:44 +0400

    HW changes in web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs - require('fs');
 
 var app = express.createServer(express.logger());
 
+var indexData = fs.readFileSync('index.html');
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(indexData.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
