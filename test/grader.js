cf4a2bc8c7ed20a36a6ef5f029ff544d0f4256d3
Varren <varren@gmail.com>
2013-07-09 05:12:22 +0400

    Final solution

--- test/grader.js
+++ test/grader.js
@@ -24,11 +24,9 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
-var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://frozen-atoll-3267.herokuapp.com/";
-var URL_BUFFER_FILE = "index-from-url.html"
+
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -62,19 +60,11 @@ var clone = function(fn) {
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
-var parceUrl = function(url){
-    
-    rest.get(url).on('complete', function(result, response){
-	fs.writeFileSync(URL_BUFFER_FILE, result);
-    });
-    return URL_BUFFER_FILE;
-}
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_page>', 'Path to index.html', clone(parceUrl), parceUrl(URL_DEFAULT))
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
